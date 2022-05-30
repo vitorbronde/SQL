33acae784e198311cd6889a94cf3d39ab7c6b2c2
Vitor Bronde <68236675+vitorbronde@users.noreply.github.com>
2022-05-30 13:54:11 -0300

    Add files via upload

--- SQL_TEMP1.sql
+++ SQL_TEMP1.sql
@@ -1,5 +1,5 @@
-DROP TABLE IF exists getninjas_temp.estudo_conversao;
-CREATE TABLE getninjas_temp.estudo_conversao AS (
+DROP TABLE IF exists temp.estudo_conversao;
+CREATE TABLE temp.estudo_conversao AS (
 with visu as (
 SELECT distinct 
     vo.profile_id ,
@@ -17,10 +17,10 @@ SELECT distinct
     last_value(vo.price) over (partition by vo.profile_id , vo.request_id  ROWS UNBOUNDED PRECEDING ) as last_price ,
     last_value(vo.current_credits) over (partition by vo.profile_id , vo.request_id ROWS UNBOUNDED PRECEDING ) as last_credit,
  (case when last_price::float > 0 then last_price::float end) / (case when last_credit::float > 0 then last_credit::float end) as razao_precos
-FROM getninjas_dispatch.visualized_offers vo
-    inner join getninjas_pivots.enriched_requests er on (vo.request_id = er.request_id)
-    inner join getninjas_intermediary.profiles_address_root AS par ON (par.profile_id = vo.profile_id)
-    join getninjas_pivots.enriched_profiles AS ep on (vo.profile_id = ep.profile_id)
+FROM visualized_offers vo
+    inner join enriched_requests er on (vo.request_id = er.request_id)
+    inner join profiles_address_root AS par ON (par.profile_id = vo.profile_id)
+    join enriched_profiles AS ep on (vo.profile_id = ep.profile_id)
     where date between '2022-01-01' and '2022-04-10'
     and ep.visible = 1 
 ),
@@ -30,7 +30,7 @@ select
 profile_id , 
 request_id , 
 count(*) as views  
-from getninjas_dispatch.visualized_offers 
+from visualized_offers 
 where date between '2022-01-01' and '2022-04-10'
 group by 1,2
 ),
@@ -43,7 +43,7 @@ select
     price_in_credits,
     rank_real_lead_in_request ,
     lead_created_at as data_compra
-from getninjas_dispatch.enriched_leads el 
+from dispatch.enriched_leads el 
 where lead_created_at between '2022-01-01' and '2022-04-10'
     and not lead_giveaway 
 )
